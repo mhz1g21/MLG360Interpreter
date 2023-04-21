503936118a6dd596c5a8acb45c87996c2837cd76
mhz1g21 <mhz1g21@soton.ac.uk>
2023-04-21 15:20:26 +0100

    added joining tiles, and import & export

--- Julio.hs
+++ Julio.hs
@@ -4,6 +4,7 @@ import System.Environment
 import Control.Exception
 import System.IO
 import Data.Map
+import GHC.IO.Handle.Internals (ioe_notReadable)
 
 
 
@@ -28,12 +29,15 @@ doEnviroment parsedProg = do
   putStrLn ("evaluated as " ++ show finalEnv)
 
 --enviroment variables
-data Enviroment = Enviroment { stack :: [Exp], symbolTable :: Map String Exp } deriving Show
+data Enviroment = Enviroment { stack :: [Exp], symbolTable :: Map String Value } deriving Show
 
 initEnv :: Enviroment
 initEnv = Enviroment [] empty
 
 type Tile = [[Bool]]
+
+data Value = TileValue Tile 
+  | IntValue Int deriving Show
 
 --evaluator
 -- ######################################################################################################################
@@ -47,8 +51,8 @@ evalExpSeq (ExpSeq e es) env =
 evalExp (Equals x e) env = evaluateEquals x e env
 evalExp (JoinH e1 e2) env = undefined
 evalExp (JoinV e1 e2) env = undefined
-evalExp (Export x y) env = undefined
-evalExp (Import x y) env = undefined
+evalExp (Export x y) env = evaluateExport x y env
+evalExp (Import x y) env = evaluateImport x y env
 evalExp (Int x ) env = undefined
 evalExp (Var x) env = undefined
 evalExp (RepeatH n e) env = undefined
@@ -59,28 +63,76 @@ evalExp (RepeatV n e) env = undefined
 evaluateEquals :: String -> Exp -> Enviroment -> Enviroment
 evaluateEquals x e env = undefined
 
-evaluateJoinH :: Exp -> Exp -> Enviroment -> Enviroment
-evaluateJoinH e1 e2 env = undefined
+--join tiles horizontally
+evaluateJoinH e1 e2 env = do
+  newEnv1 <- evalExp e1 env
+  newEnv2 <- evalExp e2 newEnv1 {stack = tail $ stack newEnv1}
+  let tile1 = head $ stack newEnv2
+  let tile2 = head $ tail $ stack newEnv2
+  case (tile1,tile2) of 
+    (TileValue t1, TileValue t2) -> case JoinTilesH t1 t2 of
+      Right newTile -> return newEnv2 {stack = TileValue newTile : drop 2 (stack newEnv2)}
+      Left err -> do
+        error $ "Tiles have different dimentions: " ++ err
+    _ -> error "expected tiles for joinH"
 
-evaluateJoinV :: Exp -> Exp -> Enviroment -> Enviroment
-evaluateJoinV e1 e2 env = undefined
+joinTilesH tile1 tile2 
+  | validateTile tile1 tile2 = Right (zipwith (++) tile1 tile2)
+  | otherwise = error "Tiles have different dimentions"
 
-evaluateExport :: Exp -> String -> Enviroment -> Enviroment
-evaluateExport x y env = undefined
+--join tiles vertically
+evaluateJoinV e1 e2 env = do
+  newEnv1 <- evalExp e1 env
+  newEnv2 <- evalExp e2 newEnv1 {stack = tail $ stack newEnv1}
+  let tile1 = head $ stack newEnv2
+  let tile2 = head $ tail $ stack newEnv2
+  case (tile1,tile2) of
+    (TileValue t1, TileValue t2) -> case joinTilesV t1 t2 of
+      Right newTile -> return newEnv2 {stack = TileValue newTile : drop 2 (stack newEnv2)}
+      Left err -> do
+        error $ "Tiles have different dimentions: " ++ err
+    _ -> error "expected tiles for joinV"
+  
+
+validateTile tile1 tile2 = allEqual (map length tile1) && allEqual (map length tile2)
+  where 
+    allEqual xs = and $ zipWith (==) xs (tail xs)
+
+joinTilesV tile1 tile1
+  | validateTile tile1 tile2 = Right (tile1 ++ tile2)
+  | otherwise = error "Tiles have different dimentions"
+
+--export tile
+evaluateExport x y env = case Map.lookup x (symbolTable env) of
+  Just (TileValue tile) -> do
+    let filePath = y <.> "tl"
+    writeTilefile filePath tile
+    return env
+  _ -> error $ "Tile variable not found: " ++ x
+
+boolToChar True = '1'
+boolToChar False = '0'
+
+tileToString = unlines . map (map boolToChar)
+
+writeTilefile filepath tile = writeFile filepath (tileToString tile)
 
 --import tile
 evaluateImport x y env = do
   let filepath = y <.> "tl"
   tile <- readFileTile filepath
-  let newSymbolTable = Map.insert x (Tile tile) (symbolTable env)
+  let newSymbolTable = Map.insert x (TileValue tile) (symbolTable env)
   return env {symbolTable = newSymbolTable}
 
 parseTile = map (map (== '1')). lines
 
-readFileTile filePath = do
-  contents <- readFile filePath
-  return $ parseTile contents
-
+readFileTile filePath = catch readSuccess noRead
+  where
+    readSuccess = do
+      file <- readFile filePath
+      return $ parseTile file
+    noRead e = do
+      error $ "File not found: " ++ filePath
 
 evaluateInt :: Int -> Enviroment -> Enviroment
 evaluateInt x env = undefined
